d831bf6e5a81011f2131244ef8842cd6fe40fa0f
Tomáš Holý <holytom5@fel.cvut.cz>
2026-09-18 16:20:32 +0200

    initial commit

---
+++ src/main/java/cz/trixi/parsexml/ParsexmlApplication.java
@@ -0,0 +1,13 @@
+package cz.trixi.parsexml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsexmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParsexmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/trixi/parsexml/ParsexmlApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.trixi.parsexml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParsexmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
