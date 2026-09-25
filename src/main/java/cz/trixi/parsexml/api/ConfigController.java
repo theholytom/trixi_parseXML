38b7c221086345ac8c0c6474ce14115147959d12
Tomáš Holý <holytom5@fel.cvut.cz>
2026-09-25 15:57:02 +0200

    adding config controller

---
+++ src/main/java/cz/trixi/parsexml/api/ConfigController.java
@@ -0,0 +1,35 @@
+package cz.trixi.parsexml.api;
+
+import cz.trixi.parsexml.connector.ClientConfig;
+import cz.trixi.parsexml.connector.ClientProperties;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClient;
+
+@RestController
+@RequestMapping("config")
+public class ConfigController {
+
+    private final ClientProperties properties;
+    private final ClientConfig config;
+    private final RestClient.Builder builder;
+
+    public ConfigController(ClientProperties properties, ClientConfig config, RestClient.Builder builder) {
+        this.properties = properties;
+        this.config = config;
+        this.builder = builder;
+    }
+
+    @GetMapping("/url")
+    public ResponseEntity<String> getResourceUrl() {
+        return ResponseEntity.ok(properties.getResourceUrl());
+    }
+
+    @PostMapping("/url")
+    public ResponseEntity<String> changeResourceUrl(@RequestBody String newUrl) {
+        properties.setResourceUrl(newUrl);
+        config.restClient(builder, properties);
+        return ResponseEntity.accepted()
+                .body("New resource URL: " + newUrl);
+    }
+}
