c045441eba069e87783fa838c751c24525c970f1
Tomáš Holý <holytom5@fel.cvut.cz>
2026-09-25 14:20:37 +0200

    adding repository annotation

--- src/main/java/cz/trixi/parsexml/persistence/repository/MunicipalityPartRepository.java
+++ src/main/java/cz/trixi/parsexml/persistence/repository/MunicipalityPartRepository.java
@@ -2,6 +2,8 @@ package cz.trixi.parsexml.persistence.repository;
 
 import cz.trixi.parsexml.persistence.entity.MunicipalityPart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MunicipalityPartRepository extends JpaRepository<MunicipalityPart, Long> {
 }
--- src/main/java/cz/trixi/parsexml/persistence/repository/MunicipalityRepository.java
+++ src/main/java/cz/trixi/parsexml/persistence/repository/MunicipalityRepository.java
@@ -2,6 +2,9 @@ package cz.trixi.parsexml.persistence.repository;
 
 import cz.trixi.parsexml.persistence.entity.Municipality;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MunicipalityRepository extends JpaRepository<Municipality, Long> {
+
 }
